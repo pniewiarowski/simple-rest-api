5c37bb3d4100bb4a690a8162879cf3179c1692cd
CowMan <pawelniewiarowski2001@gmail.com>
2022-08-15 11:27:05 +0200

    Use default middlewares

--- app/run.go
+++ app/run.go
@@ -2,14 +2,20 @@ package app
 
 import (
 	"fmt"
-	"github.com/gofiber/fiber/v2"
-	"github.com/pniewiarowski/simple-rest-api/router"
 	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/pniewiarowski/simple-rest-api/router"
 )
 
 func Run(port int) {
 	app := fiber.New()
 	api := app.Group("/api")
+
+	app.Use(logger.New())
+	app.Use(csrf.New())
 
 	router.SetupBook(api)
 	router.SetupAuthor(api)
